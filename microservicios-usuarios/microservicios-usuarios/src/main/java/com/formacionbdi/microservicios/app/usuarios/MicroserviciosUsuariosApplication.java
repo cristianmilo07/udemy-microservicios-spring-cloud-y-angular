04d782b0c8abce451ffc204d56b5c6e481e62943
Cristian camilo <cristianmilo07@gmail.com>
2024-02-29 08:18:34 -0500

    Todo esta corregido y corriendo

--- microservicios-usuarios/microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
+++ microservicios-usuarios/microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
@@ -2,10 +2,11 @@ package com.formacionbdi.microservicios.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviciosUsuariosApplication {
 
 	public static void main(String[] args) {
